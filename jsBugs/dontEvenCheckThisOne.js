5b64d404ec31d931329cfe722489fa8bb23321a4
ihollander <ianholla@gmail.com>
2018-11-30 13:00:49 -0500

    Done.

--- jsBugs/dontEvenCheckThisOne.js
+++ jsBugs/dontEvenCheckThisOne.js
@@ -3,7 +3,7 @@ p.textContent =
   'When you click the following buttons, the alert should match the number on the button.'
 document.body.appendChild(document.createElement('br'))
 document.body.appendChild(p)
-for (var i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
   var button = document.createElement('button')
   button.textContent = 'Button ' + i
   document.body.appendChild(button)
