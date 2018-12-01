6accfb96e2a4babc255484e80fbadc3297df010a
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-12-01 16:59:15 -0500

    bug free

--- jsBugs/impressivelyCorrect.js
+++ jsBugs/impressivelyCorrect.js
@@ -1,15 +1,34 @@
-const students = [
-  { name: 'Bobby', hometown: 'Pittsburgh', labsComplete: 801 },
-  { name: 'Sally', hometown: 'New York', labsComplete: 398 },
-  { name: 'Sammy', hometown: 'Pittsburgh', labsComplete: 550 },
-  { name: 'Annette', hometown: 'Los Angeles', labsComplete: 896 },
-  { name: 'Joey', hometown: 'Tampa Bay', labsComplete: 0 }
+const students = [{
+    name: 'Bobby',
+    hometown: 'Pittsburgh',
+    labsComplete: 801
+  },
+  {
+    name: 'Sally',
+    hometown: 'New York',
+    labsComplete: 398
+  },
+  {
+    name: 'Sammy',
+    hometown: 'Pittsburgh',
+    labsComplete: 550
+  },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles',
+    labsComplete: 896
+  },
+  {
+    name: 'Joey',
+    hometown: 'Tampa Bay',
+    labsComplete: 0
+  }
 ]
 
 const totalLabsComplete = students => {
   return students.reduce((total, student) => {
-    return labsComplete + total
-  })
+    return total + student.labsComplete
+  }, 0)
 }
 
 const averageLabsComplete = students => {
